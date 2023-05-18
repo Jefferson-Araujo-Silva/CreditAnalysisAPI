7899fe96c93366df78b15ba0562ea7aa4b5f8fa8
Jefferson <jefferson.silva@jazztech.com.br>
2023-05-18 11:40:47 -0300

    Criando teste should_create_new_credit_analysis_response E: should_return_where_client_where_id_client_is_1f017304_c7cf_45cb_e2c_5f6ce1f22560

--- credity-analysis/credit-analysis/src/test/java/com/app/creditanalysis/service/CreditAnalysisServiceTest.java
+++ credity-analysis/credit-analysis/src/test/java/com/app/creditanalysis/service/CreditAnalysisServiceTest.java
@@ -2,6 +2,7 @@ package com.app.creditanalysis.service;
 
 import com.app.creditanalysis.apicreditanalysis.ClientApiCreditAnalysis;
 import com.app.creditanalysis.apicreditanalysis.clientdto.ClientDto;
+import com.app.creditanalysis.controller.request.CreditAnalysisRequest;
 import com.app.creditanalysis.controller.response.CreditAnalysisResponse;
 import com.app.creditanalysis.mapper.CreditAnalysisEntityMapper;
 import com.app.creditanalysis.mapper.CreditAnalysisEntityMapperImpl;
@@ -24,7 +25,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class CreditAnalysisServiceTest {
@@ -40,28 +43,73 @@ public class CreditAnalysisServiceTest {
     private CreditAnalysisRepository creditAnalysisRepository;
     @Spy
     private CreditAnalysisEntityMapper creditAnalysisEntityMapper = new CreditAnalysisEntityMapperImpl();
+   @Spy CreditAnalysisMapper creditAnalysisMapper = new CreditAnalysisMapperImpl();
     @Spy
     private CreditAnalysisResponseMapper creditAnalysisResponseMapper = new CreditAnalysisResponseMapperImpl();
-    private final CreditAnalysisMapper creditAnalysisMapper = new CreditAnalysisMapperImpl();
 
     @Test
-    public void should_create_new_credit_analysis_respose(a){}
+    public void should_create_new_credit_analysis_response() {
+        ClientDto clientDto = clientFactory();
+        when(creditAnalysisApi.getClientById(idClientArgumentCaptor.capture())).thenReturn(clientDto);
+        when(creditAnalysisRepository.save(creditAnalysisEntityArgumentCaptor.capture()))
+                .thenReturn(creditAnalysisEntityFactory());
 
-    public static CreditAnalysisResponse creditAnalysisFactory() {
+        final CreditAnalysisRequest request = creditAnalysisRequestFactory();
+        final CreditAnalysisResponse response = creditAnalysisService.creditAnalising(request);
+
+        assertNotNull(response);
+        assertNotNull(response.id());
+        CreditAnalysisEntity entity = creditAnalysisEntityArgumentCaptor.getValue();
+        assertEquals(response.annualInterest(), entity.getAnnualInterest());
+        assertEquals(response.approved(), entity.getApproved());
+        assertEquals(response.clientId(), entity.getClientId());
+        assertEquals(response.withdrawalLimitValue(), entity.getWithdrawlLimitValue());
+        assertEquals(response.approvedLimit(), entity.getApprovedLimit());
+    }
+
+    @Test
+    public void should_return_where_client_where_id_client_is_1f017304_c7cf_45cb_e2c_5f6ce1f22560(){
+        when(creditAnalysisRepository.findFirstByClientId(idClientArgumentCaptor.capture())).thenReturn(creditAnalysisEntityFactory());
+
+        final CreditAnalysisEntity entity = creditAnalysisEntityFactory();
+        final CreditAnalysisResponse response = creditAnalysisService
+                .findAnalysisByIdClient(UUID.fromString("1f017304-c7cf-45cb-8e2c-5f6ce1f22560"));
+        assertEquals(entity.getAnnualInterest(), response.annualInterest());
+        assertEquals(entity.getApproved(), response.approved());
+        assertEquals(entity.getApprovedLimit(), response.approvedLimit());
+        assertEquals(entity.getWithdrawlLimitValue(), response.withdrawalLimitValue());
+    }
+
+    public static CreditAnalysisResponse creditAnalysisResponseFactory() {
         return CreditAnalysisResponse.builder()
                 .id(UUID.randomUUID())
                 .date(LocalDateTime.now())
-                .approved(true)
-                .approvedLimit(new BigDecimal("30.00"))
-                .withdrawalLimitValue(new BigDecimal("3.00"))
-                .annualInterest(15.0)
-                .clientId(UUID.fromString("8cecf090-d8c5-46af-88f9-4892d19a2c17"))
+                .approved(true).approvedLimit(new BigDecimal("30.00"))
+                .withdrawalLimitValue(new BigDecimal("3.00")).annualInterest(15.0)
+                .clientId(UUID.fromString("1f017304-c7cf-45cb-8e2c-5f6ce1f22560"))
                 .build();
     }
-    public static ClientDto clientFactory(){
-        return ClientDto.builder()
-                .cpf("53887957806")
-                .id(UUID.fromString("8cecf090-d8c5-46af-88f9-4892d19a2c17"))
+    public static CreditAnalysisRequest creditAnalysisRequestFactory(){
+        return CreditAnalysisRequest.builder()
+                .requestedAmount(new BigDecimal("5.00"))
+                .monthlyIncome(new BigDecimal("100.0"))
+                .clientId(UUID.fromString("1f017304-c7cf-45cb-8e2c-5f6ce1f22560"))
+                .build();
+    }
+    public static CreditAnalysisEntity creditAnalysisEntityFactory() {
+        return CreditAnalysisEntity.builder()
+                .date(LocalDateTime.now())
+                .approved(true).approvedLimit(new BigDecimal("30.00"))
+                .withdrawalLimitValue(new BigDecimal("3.00")).annualInterest(15.0)
+                .clientId(UUID.fromString("1f017304-c7cf-45cb-8e2c-5f6ce1f22560"))
+                .monthlyIncome(new BigDecimal("100.00"))
+                .requestedAmount(new BigDecimal("5.00"))
+                .annualInterest(15.0)
+                .build();
+    }
+    public static ClientDto clientFactory() {
+        return ClientDto.builder().cpf("53887957806").
+                id(UUID.fromString("1f017304-c7cf-45cb-8e2c-5f6ce1f22560"))
                 .build();
     }
 }
