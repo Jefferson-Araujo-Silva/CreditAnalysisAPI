95e69f83fb4059d9b77cec2a4a5e7cab7061f3f4
Jefferson <jefferson.silva@jazztech.com.br>
2023-05-22 09:58:56 -0300

    merging

--- credit-analysis/credit-analysis-api/src/main/java/com/app/creditanalysis/exception/RequestedAmountExceedsMonthlyIncome.java
+++
@@ -1,7 +0,0 @@
-package com.app.creditanalysis.exception;
-
-public class RequestedAmountExceedsMonthlyIncome extends RuntimeException {
-    public RequestedAmountExceedsMonthlyIncome(String message) {
-        super(message);
-    }
-}
