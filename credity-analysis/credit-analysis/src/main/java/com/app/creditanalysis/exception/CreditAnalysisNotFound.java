0a12011b6de910fa99a0062ff17c1cf3c3c75e5e
Jefferson <jefferson.silva@jazztech.com.br>
2023-05-23 10:28:02 -0300

    organizated project

--- credity-analysis/credit-analysis/src/main/java/com/app/creditanalysis/exception/CreditAnalysisNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.app.creditanalysis.exception;
-
-public class CreditAnalysisNotFound extends RuntimeException {
-    public CreditAnalysisNotFound(String message) {
-        super(message);
-    }
-}
